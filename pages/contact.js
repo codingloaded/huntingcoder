936b9f30e8035f671a712b6e73d7ea542bd63b9c
codingloaded <144691055+codingloaded@users.noreply.github.com>
2023-11-20 09:30:19 +0530

    contact page and style added

--- pages/contact.js
+++ pages/contact.js
@@ -1,11 +1,32 @@
-import React from 'react'
+import React from 'react';
+import styles from '@/styles/Contact.module.css';
 
 function Contact() {
   return (
-    <div>
-     <h2>This is contact page</h2> 
+    <div className={styles.container}>
+     <h1>Contact us now</h1>
+          <form>
+            <div className={styles.mb3}>
+              <label htmlFor="name" className={styles.formlabel}>Enter your name:- </label>
+              <input type="text" className="form-control" id="name" name = "name" />
+            </div>
+            <div className={styles.mb3}>
+              <label htmlFor="phone" className={styles.formlabel}>Enter your phone:- </label>
+              <input type="phone" className="form-control" id="phone" name = "phone" />
+            </div>
+            <div className={styles.mb3}>
+              <label htmlFor="email" name ="email" className={styles.formlabel}>Email address:- </label>
+              <input type="email" className="form-control" id="email" name ="email" />
+            </div>
+            <div className={styles.mb3}>
+              <label htmlFor="msg" className={styles.formlabel}>leave your message:- </label>
+              <textarea id ="msg" name ="des" className="form-control" ></textarea>
+            </div>
+            <button type="submit" className="btn btn-primary">Submit</button>
+      </form>
+
     </div>
   )
 }
 
-export default Contact
+export default Contact;
